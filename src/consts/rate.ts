49f0f01ea79389cc543fca4c1e54e1cfc2f4fe24
RYOTA-F <ry.fj0304@gmail.com>
2024-01-14 11:09:19 +0900

    :sparkles: SNS 実装

---
+++ src/consts/rate.ts
@@ -0,0 +1,2 @@
+// 百分率
+export const RATE = 100 as const
---
+++ src/methods/sns/calculationPresentValue.ts
@@ -0,0 +1,45 @@
+import { RATE } from '../../consts/rate'
+
+/**
+ * 指定した桁を四捨五入
+ */
+export const roundWithScale = (value: number, scale: number) => {
+  return Math.round(value * 10 ** scale) / 10 ** scale
+}
+
+/**
+ * 現在価値を計算
+ */
+export const calculationPresentValue = (
+  yearsCount: number,
+  discountPercentage: number,
+  yearsCashFrow: number,
+) => {
+  // 割引率
+  const discountRate = discountPercentage / RATE
+  // 年数ごとの配列
+  const yearsArray = Array.from({ length: yearsCount }, (_, i) => i + 1)
+
+  // ディスカウントレートの配列
+  const discountRateArray = yearsArray.map(
+    (year) => 1 / (1 + discountRate) ** year,
+  )
+
+  // 単年度の現在価値
+  const presentValueArray = discountRateArray.map(
+    (discountRate, i) => yearsCashFrow * discountRate,
+  )
+
+  const totalPresentValue = presentValueArray.reduce((a, b) => a + b)
+
+  return {
+    yearsArray,
+    discountRateArray: discountRateArray.map((discountRate) =>
+      roundWithScale(discountRate, 3),
+    ),
+    presentValueArray: presentValueArray.map((presentValue) =>
+      roundWithScale(presentValue, 1),
+    ),
+    totalPresentValue: roundWithScale(totalPresentValue, 1),
+  }
+}
