af712e5cb841bd0610f45c5f4bf8b17096357806
RYOTA-F <ry.fj0304@gmail.com>
2024-01-22 13:29:07 +0900

    :recycle: Layout 修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,19 @@ import { Box, Card, Space } from '@mantine/core'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Box>
-        <Link href="/finance/npv" className="w-1/2">
-          <Card shadow="xl">
-            <h2 className="text-2xl font-bold text-center text-sub">NPV</h2>
-          </Card>
-        </Link>
-        <Space h="lg" />
+    <Box w={400} className="my-0 mx-auto">
+      <Link href="/finance/npv">
+        <Card shadow="xl">
+          <h2 className="text-2xl font-bold text-center text-sub">NPV</h2>
+        </Card>
+      </Link>
+      <Space h="lg" />
 
-        <Link href="/finance/irr" className="w-1/2">
-          <Card shadow="xl">
-            <h2 className="text-2xl font-bold text-center text-sub">IRR</h2>
-          </Card>
-        </Link>
-      </Box>
-    </main>
+      <Link href="/finance/irr">
+        <Card shadow="xl">
+          <h2 className="text-2xl font-bold text-center text-sub">IRR</h2>
+        </Card>
+      </Link>
+    </Box>
   )
 }
