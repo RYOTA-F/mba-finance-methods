57c47d6fd483cf74a42d75b4de8a648270f217ab
RYOTA-F <ry.fj0304@gmail.com>
2024-01-27 12:25:46 +0900

    :recycle: コメント追加

--- src/methods/irr/irr.ts
+++ src/methods/irr/irr.ts
@@ -1,5 +1,10 @@
 import { roundWithScale } from '@/methods/common/round'
 
+/**
+ * 内部収益率を計算
+ * 高次代数方程式: 漸近法で算出 (解析法では算出不可)
+ * https://envision.microsoft.com/ja-JP/tokyo?wt.mc_ID=Envision2023_esc_corp_em_oo_fld_tokyo_city_homepage%20
+ */
 export const calculationIRR = (cashFlows: number[]) => {
   // 許容範囲
   const tolerance = 1.0e-7
