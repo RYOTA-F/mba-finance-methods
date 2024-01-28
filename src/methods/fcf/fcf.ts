38e6a75e061514dec4d9dfb79356b8e2db30bfa9
RYOTA-F <ry.fj0304@gmail.com>
2024-01-28 19:33:35 +0900

    :sparkles: calculationFCF 実装

--- src/methods/fcf/fcf.ts
+++ src/methods/fcf/fcf.ts
@@ -1,9 +1,26 @@
+export interface InputData {
+  operationgIncome: number
+  taxAmount: number
+  depreciationExpense: number
+  investmentAmount: number
+  wc: number
+}
+
+export interface OutputData extends InputData {
+  fcf: number
+}
+
 /**
  * フリーキャッシュフローを計算
  */
-export const calculationFCF = (
-  cashFlows: number[],
-  tax: number,
-  wacc: number,
-  investmentAmount: number
-) => {}
+export const calculationFCF = (inputData: InputData[]): OutputData[] => {
+  return inputData.map((data) => ({
+    ...data,
+    fcf:
+      data.operationgIncome -
+      data.taxAmount +
+      data.depreciationExpense -
+      data.investmentAmount -
+      data.wc,
+  }))
+}
