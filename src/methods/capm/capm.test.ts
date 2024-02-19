48089b9b80e66a1f7d83e94b7db6c7cf119e6176
RYOTA-F <ry.fj0304@gmail.com>
2024-02-19 12:40:59 +0900

    :sparkles: calculationFCF テスト追加

---
+++ src/methods/capm/capm.test.ts
@@ -0,0 +1,10 @@
+import { describe, test, expect } from 'vitest'
+// import { calculationCAPM } from './capm'
+
+describe('calculationFCF', () => {
+  describe('IEF Day3', () => {
+    test('CAPM が正しく出力される', () => {
+      expect(true).toEqual(true)
+    })
+  })
+})
